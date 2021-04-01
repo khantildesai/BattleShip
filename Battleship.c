0ac7cdbc3d5adf224f5168de99aa379443fa9aba
BaguetteBen <benjamin.smieja@gmail.com>
2021-04-01 15:56:43 -0400

    Create draw grid function + entire screen clear

--- Battleship.c
+++ Battleship.c
@@ -32,16 +32,138 @@
 #define RESOLUTION_X 320
 #define RESOLUTION_Y 240
 
-
-
-
+/*Game constants*/
+#define GRID_WIDTH 23
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
+void Setup();
+void swap(int *, int *);
+void clear_screen();
+void draw_line(int x0, int y0, int x1, int y1, short int line_color);
+void DrawGrid();
+
+volatile int pixel_buffer_start; // global variable
 
 int main(void)
 {
-    
+    volatile int *pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+    /* Read location of the pixel buffer from the pixel buffer controller */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+
+    clear_screen();
+    DrawGrid();
+    //draw_line(0, 0, 150, 150, 0x001F); // this line is blue
+}
+
+void plot_pixel(int x, int y, short int line_color)
+{
+    *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
+}
+
+//Clears the entire screen by plotting black pixels
+void clear_screen()
+{
+    for (int x = 0; x < 320; x++)
+    {
+        for (int y = 0; y < 240; y++)
+        {
+            plot_pixel(x, y, 0x0000);
+        }
+    }
+    return;
+}
+
+//Draws a line segment
+void draw_line(int x0, int y0, int x1, int y1, short int line_color)
+{
+
+    //checks if y increases faster than x
+    bool is_steep = abs(y1 - y0) > abs(x1 - x0);
+
+    //If y increases too fast swap x and y, so x will increase faster
+    if (is_steep)
+    {
+        swap(&x0, &y0);
+        swap(&x1, &y1);
+    }
+
+    //Checks if X0 is greater than x1
+    if (x0 > x1)
+    {
+        //Swaps x0 and x1
+        swap(&x0, &x1);
+        swap(&y0, &y1);
+    }
+
+    int deltax = x1 - x0;
+    int deltay = abs(y1 - y0);
+    int error = -1 * (deltax / 2);
+    int y = y0;
+
+    int y_step;
+
+    //Calculates if y should be increasing or decreasing
+    if (y0 < y1)
+    {
+        y_step = 1;
+    }
+    else
+    {
+        y_step = -1;
+    }
+
+    //Iterates through number of xs
+    for (int x = x0; x <= x1; x++)
+    {
+        //if steep y and x are reversed (Guards for vertical lines)
+        if (is_steep)
+        {
+            plot_pixel(y, x, line_color);
+        }
+        else // Not steep normal x and y
+        {
+            plot_pixel(x, y, line_color);
+        }
+
+        //Recalculate Error by adding delta y
+        error = error + deltay;
+
+        //Check if error is positive
+        if (error >= 0)
+        {
+            //Pos Error => add y_step to y and subtract dela x from error
+            y += y_step;
+            error -= deltax;
+        }
+    }
+}
+
+//Swaps two integers by pass by reference
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+void Setup()
+{
+}
+
+void DrawGrid()
+{
+    //Draws Rows
+    for (int row = 0; row < 10; row++)
+    { 
+        draw_line(40,24+24*row,280,24 +24*row,GREY);
+    }
+    //Draws columns
+    for (int col = 0; col < 11; col++)
+    {
+        draw_line(40+24*col,0,40 + 24*col,RESOLUTION_Y - 1,GREY);
+    }
 }
